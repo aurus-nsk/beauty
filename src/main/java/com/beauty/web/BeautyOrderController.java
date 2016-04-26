071d2e7bb9b3496c7fbf9a4f403bba2f88003e1c
unknown <s.chepurnov@NSB-3487.kfins.ru>
2016-04-26 10:52:58 +0600

    spring hibernate angular

---
+++ src/main/java/com/beauty/web/BeautyOrderController.java
@@ -0,0 +1,12 @@
+package com.beauty.web;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.beauty.domain.BeautyOrder;
+
+@RequestMapping("/beautyorders")
+@Controller
+@RooWebScaffold(path = "beautyorders", formBackingObject = BeautyOrder.class)
+public class BeautyOrderController {
+}
