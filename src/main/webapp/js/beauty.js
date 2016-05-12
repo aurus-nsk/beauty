bde3b78272ab6e3673c1437f762ef8fd894ad9ed
unknown <s.chepurnov@NSB-3487.kfins.ru>
2016-05-12 11:31:36 +0600

    translate

--- src/main/webapp/js/beauty.js
+++ src/main/webapp/js/beauty.js
@@ -1,7 +1,6 @@
-
 $( document ).ready(function() {
 	
-	//additional fields
+	//constants
 	var priceForKg = 0.016;
 	var currencyPurchase = $("c_com_beauty_domain_BeautyOrder_currency");
 	var currencyDelivery = 39,38;
@@ -24,19 +23,35 @@ $( document ).ready(function() {
 	var totalSumDollars = $("c_com_beauty_domain_BeautyOrder_totalSumDollars");
 	var totalSumRubles = $("c_com_beauty_domain_BeautyOrder_totalSumRubles");
 	
+	//listeners
+	currencyPurchase.addEventListener('change', calculate, false);
+	//var priceForKg = 0.016;
+	//var currencyDelivery = 39,38;
 	
-	//click 
-	//TODO: create function
-	totalDollars.val( (Number(quantity.val()) * Number(priceSaleOrg.val())) + Number(taxDollars.val()) + Number(deliveryUSA.val()) );
-	totalRubles.val( Number(totalDollars.val()) * Number(currencyPurchase.val()) );
-	deliveryRussiaDollars.val( Number(weight.val()) * Number(priceForKg) );
-	deliveryRussiaRubles.val( Number(deliveryRussiaDollars.val()) * Number(currencyDelivery));
-	totalSumDollars.val( Number(totalDollars.val()) + Number(deliveryRussiaDollars.val()) );
-	totalSumRubles.val( Number(totalRubles.val()) + Number(deliveryRussiaRubles.val()) );
+	shopName.addEventListener('change', calculate, false);
+	productName.addEventListener('change', calculate, false);
+	colour.addEventListener('change', calculate, false);
+	sizeName.addEventListener('change', calculate, false);
+	quantity.addEventListener('change', calculate, false);
+	priceOrigin.addEventListener('change', calculate, false);
+	priceSaleOrg.addEventListener('change', calculate, false);
+	taxDollars.addEventListener('change', calculate, false);
+	deliveryUSA.addEventListener('change', calculate, false);
+	totalDollars.addEventListener('change', calculate, false);
+	totalRubles.addEventListener('change', calculate, false);
+	weight.addEventListener('change', calculate, false);
+	deliveryRussiaDollars.addEventListener('change', calculate, false);
+	deliveryRussiaRubles.addEventListener('change', calculate, false);
+	totalSumDollars.addEventListener('change', calculate, false);
+	totalSumRubles.addEventListener('change', calculate, false);
 	
-	/* TODO: add listener
-	$("#other").click(function() {
-		  $( ".target" ).change();
-	});
-	*/ 
+	function calculate() {
+		totalDollars.val( (Number(quantity.val()) * Number(priceSaleOrg.val())) + Number(taxDollars.val()) + Number(deliveryUSA.val()) );
+		totalRubles.val( Number(totalDollars.val()) * Number(currencyPurchase.val()) );
+		deliveryRussiaDollars.val( Number(weight.val()) * Number(priceForKg) );
+		deliveryRussiaRubles.val( Number(deliveryRussiaDollars.val()) * Number(currencyDelivery));
+		totalSumDollars.val( Number(totalDollars.val()) + Number(deliveryRussiaDollars.val()) );
+		totalSumRubles.val( Number(totalRubles.val()) + Number(deliveryRussiaRubles.val()) );
+	}
+	
 });
