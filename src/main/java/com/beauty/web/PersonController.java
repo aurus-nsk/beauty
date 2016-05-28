70c41e5ee7208c12d55152b3322424018512c580
aurus-nsk <aurus7@mail.ru>
2016-05-28 16:17:47 +0700

    initDB, buttons update delete create for admin only

--- src/main/java/com/beauty/web/PersonController.java
+++ src/main/java/com/beauty/web/PersonController.java
@@ -1,11 +1,29 @@
 package com.beauty.web;
-import com.beauty.domain.Person;
+import java.util.Date;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.beauty.domain.Person;
 
 @RequestMapping("/people")
 @Controller
 @RooWebScaffold(path = "people", formBackingObject = Person.class)
 public class PersonController {
+	
+	@RequestMapping("/initDB")
+	public String initDB () {
+		Person admin = new Person();
+		admin.setName("Александра");
+		admin.setMiddleName("Анатольевна");
+		admin.setSurname("Геращенко");
+		admin.setUsername("admin");
+		admin.setPassword("123456");
+		admin.setDateCreate(new Date());
+		
+		admin.persist();
+		
+		return "redirect:/";
+	}
 }
